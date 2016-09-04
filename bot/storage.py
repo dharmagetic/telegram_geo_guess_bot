17ac48d27be54ee95008f56a715616d872eb8a3a
Evgeniy <dharmagetic@gmail.com>
2016-09-04 16:30:04 +0300

    state

--- bot/storage.py
+++ bot/storage.py
@@ -3,12 +3,12 @@ import shelve
 
 def set_user_state(chat_id, state):
     d = shelve.open('shelve.db')
-    d[chat_id] = state
+    d[str(chat_id)] = state
     d.close()
 
 
 def get_user_state(chat_id):
     d = shelve.open('shelve.db')
-    state = d[chat_id]
+    state = d[str(chat_id)]
     d.close()
     return state
