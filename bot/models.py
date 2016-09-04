3c4a112577c5ac4b1b435d52d64f156b518962a0
Evgeniy <dharmagetic@gmail.com>
2016-09-04 16:54:29 +0300

    state

--- bot/models.py
+++ bot/models.py
@@ -27,6 +27,7 @@ class Country(models.Model):
     name = models.CharField(verbose_name=u"Cтрана", max_length=255, db_index=True)
     capital = models.CharField(verbose_name=u"Столица", max_length=255, db_index=True)
     random = RandomGeoObjectManager()
+    objects = models.Manager()
 
     def __unicode__(self):
         return u"%s – %s" % (self.name, self.capital)
